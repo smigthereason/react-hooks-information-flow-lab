815931fc693e68d283d89a204fe6668b9b21e569
victor@victor <victor.dmaina@gmail.com>
2024-04-30 14:24:21 +0300

    add and update files

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header ({onDarkModeClick}) {
+    return (
+        <header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          Toggle Dark Mode
+        </button>
+      </header>
+    )
+}
+
+export default Header;
